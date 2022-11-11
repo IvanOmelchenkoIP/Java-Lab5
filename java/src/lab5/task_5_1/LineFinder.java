8fc5a8ad2f951b28187928eb6e0a287db6c3f091
IvanOmelchenkoIP <io25112002@gmail.com>
2022-11-11 21:59:08 +0200

    Implement LineFinder public method
    Implement public method mostWordsLine of LineFinder class which finds a
    line with most words in a string, for that also implement private method
    wordCount that counts an amount of words in a string

--- java/src/lab5/task_5_1/LineFinder.java
+++ java/src/lab5/task_5_1/LineFinder.java
@@ -1,7 +1,19 @@
 package lab5.task_5_1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class LineFinder {
-	public String mostWordsLine(String data) {
-		return "";
+	public String mostWordsLine(String str) {
+		ArrayList<String> lines = new ArrayList<String>(Arrays.asList(str.split("\n")));
+		String longest = lines.remove(0);
+		for (String line : lines) {
+			if (wordCount(longest) < wordCount(line)) longest = line;
+		}
+		return longest;
+	}
+	
+	private int wordCount(String str) {
+		return str.split(" ").length;
 	}
 }
