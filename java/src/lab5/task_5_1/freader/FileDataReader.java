25ffa0a7bbea1b55295895b9e452e7f279c1df90
IvanOmelchenkoIP <io25112002@gmail.com>
2022-11-12 21:26:06 +0200

    Implement read method of FileDataReader
    Implement read method of FileDataReader using object streams to read
    string of data as requested in the task

--- java/src/lab5/task_5_1/freader/FileDataReader.java
+++ java/src/lab5/task_5_1/freader/FileDataReader.java
@@ -1,9 +1,21 @@
 package lab5.task_5_1.freader;
 
-import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 
 public class FileDataReader {
-	public String read(String filepath) {
-		return "";
+
+	public String read(String filepath) throws IOException {
+		ObjectInputStream os = null;
+		try {
+			os = new ObjectInputStream(new FileInputStream(filepath));
+			return os.readUTF();
+		} finally {
+			if (os != null) {
+				os.close();
+			}
+		}
 	}
+
 }
