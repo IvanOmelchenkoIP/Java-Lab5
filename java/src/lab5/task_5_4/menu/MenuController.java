883ea4b7ae6770fb4cc664a81f886e4821ec3f24
IvanOmelchenkoIP <io25112002@gmail.com>
2022-11-19 20:33:49 +0200

    Implement MenuController
    Implement MenuController that uses previously implemented classes

--- java/src/lab5/task_5_4/menu/MenuController.java
+++ java/src/lab5/task_5_4/menu/MenuController.java
@@ -1,5 +1,60 @@
 package lab5.task_5_4.menu;
 
+import lab5.task_5_4.fs.FileUrlSystem;
+import lab5.task_5_4.scanner.InputScanner;
+
 public class MenuController {
 
+	private boolean runFlag;
+
+	private MenuModel model;
+	private MenuView view;
+
+	public MenuController(MenuModel model, MenuView view) {
+		this.model = model;
+		this.view = view;
+	}
+
+	public void launch(InputScanner scanner, FileUrlSystem fs) {
+		runFlag = true;
+		model.addControllers(fs);
+
+		while (runFlag) {
+			view.showMessage(MenuView.MENU);
+			view.showMessage(MenuView.MENU_INPUT_FIELD);
+			String input = scanner.scanInput();
+			int state = model.processCommand(input);
+			processMenuState(state, scanner);
+		}
+		scanner.close();
+	}
+
+	private void processMenuState(int state, InputScanner scanner) {
+		switch (state) {
+		case MenuStates.STATUS_INCORRECT_COMMAND -> view.showMessage(MenuView.COMMAND_ERROR);
+		case MenuStates.STATUS_EXIT -> runFlag = false;
+		case MenuStates.STATUS_TAGS_BY_ALPHABET -> tagsByAlphabet(scanner);
+		case MenuStates.STATUS_TAGS_BY_OCCURENCE -> tagsByOccurence(scanner);
+		}
+	}
+
+	private void tagsByAlphabet(InputScanner scanner) {
+		view.showMessage(MenuView.URL_INPUT_FIELD);
+		String url = scanner.scanInput();
+		try {
+			model.tagsByAlphabet(url);
+		} catch (Exception exception) {
+			view.showException(MenuView.FILE_ERROR, exception);
+		}
+	}
+
+	private void tagsByOccurence(InputScanner scanner) {
+		view.showMessage(MenuView.URL_INPUT_FIELD);
+		String url = scanner.scanInput();
+		try {
+			model.tagsByOccurence(url);
+		} catch (Exception exception) {
+			view.showException(MenuView.FILE_ERROR, exception);
+		}
+	}
 }
