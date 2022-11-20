9150d9ce3d3ac9fcd176a28f6dc0539ad3e64e3f
IvanOmelchenkoIP <io25112002@gmail.com>
2022-11-20 12:51:51 +0200

    Fix output
    Fix console output of TagAnalyserView class

--- java/src/lab5/task_5_4/tags/TagAnalyserView.java
+++ java/src/lab5/task_5_4/tags/TagAnalyserView.java
@@ -6,13 +6,14 @@ import java.util.Map;
 
 public class TagAnalyserView {
 
-	public final static String SORTED_BY_ALPHABET = "The tags sorted by alphabet: \n";
-	public final static String SORTED_BY_OCCURENCE = "The tags sorted by occurence: \n";
+	public final static String SORTED_BY_ALPHABET = "The tags sorted by alphabet:";
+	public final static String SORTED_BY_OCCURENCE = "The tags sorted by occurence:";
 	public final static String EMPTY = "There were no tags found on a page!\n";
 
 	public void showSortedTags(HashMap<String, Integer> tags, String msgSuccess, String msgError) {
 		if (tags.size() == 0) {
 			System.out.println(msgError);
+			return;
 		}
 
 		System.out.println(msgSuccess);
@@ -20,5 +21,6 @@ public class TagAnalyserView {
 		for (Map.Entry<String, Integer> entry : list) {
 			System.out.println("Tag: \"" + entry.getKey() + "\", Occurence amount: " + entry.getValue());
 		}
+		System.out.println();
 	}
 }
--- java/src/lab5/task_5_4/tags/comparators/MapValueComparator.java
+++ java/src/lab5/task_5_4/tags/comparators/MapValueComparator.java
@@ -2,13 +2,12 @@ package lab5.task_5_4.tags.comparators;
 
 import java.util.Comparator;
 import java.util.Map;
-import java.util.Map.Entry;
 
-public class MapValueComparator implements Comparator<Map.Entry<String, Integer>>{
+public class MapValueComparator implements Comparator<Map.Entry<String, Integer>> {
 
 	@Override
-	public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
-		return o1.getValue().compareTo(o2.getValue());
+	public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+		return (o1.getValue().compareTo(o2.getValue()));
 	}
 
 }
